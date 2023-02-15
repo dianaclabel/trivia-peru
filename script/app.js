38e05da949cff991165d25d2c10a2fa537131242
Diana Clabel <67984989+dianaclabel@users.noreply.github.com>
2023-02-15 02:02:47 +0000

    add file app

---
+++ script/app.js
@@ -0,0 +1,28 @@
+
+function seleccion() {
+  const limaInput = document.getElementById("lima").checked;
+  const cuzcoInput = document.getElementById("cuzco").checked;
+  const arequipaInput = document.getElementById("arequipa").checked;
+
+  const veintidosInput = document.getElementById("veintidos").checked;
+  const veinteInput = document.getElementById("veinte");
+  const veinticuatroInput = document.getElementById("veinticuatro").checked;
+
+  const cajamarcaInput = document.getElementByIdElementById("cajamarca").checked;
+  const icaInput = document.getElementById("ica").checked;
+  const punoInput = document.getElementById("puno").checked;
+
+
+
+  if (limaInput.checked === true) {
+    alert("repuesta Correcta");
+  } else{
+    alert("respuesta incorrecta")
+  };
+
+
+
+
+
+
+}
